ad3f14d972056603a4d8bc9428fffa122fa7910d
Dima <cherepkov.2025@mail.ru>
2025-01-15 12:14:45 +0500

    Data Types Topic

---
+++ LABS/LAB1/datatypes.py
@@ -0,0 +1,17 @@
+x = 5
+print(type(x))
+x1 = "Hello World" # string
+x2 = 20 # int
+x3 = 20.5 # float
+x4 = 1j #  complex
+x5 = ["apple", "banana", "cherry"] # dictionary
+x6 = ("apple", "banana", "cherry") # tuple
+x7 = range(6) # range
+x8 = {"name" : "John", "age" : 36} # dictionary
+x9 = {"apple", "banana", "cherry"} # set
+x10 = frozenset({"apple", "banana", "cherry"}) # frozenset
+x11 = True # bool
+x12 = b"Hello" # bytes
+x13 = bytearray(5) # bytearray
+x14 = memoryview(bytes(5)) # memoreview
+x15 = None # NoneType
