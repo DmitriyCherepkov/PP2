60e3206fb246d5f512a4faa1e1f1c3560522bc7f
Dima <cherepkov.2025@mail.ru>
2025-01-20 18:55:42 +0500

    Operators topic

---
+++ LABS/LAB2/operators.py
@@ -0,0 +1,12 @@
+print(10+5, 10-5, 10*2, 10/2, 10%2, 10**2, 10//2)
+
+x = 15
+x+= 5
+arr = [1,2,3,4,5]
+
+if x < 5 and  x < 10: print(x)
+
+print(x is 5, isinstance(x, int), 5 in arr)
+print(5 & 2, 5 | 2, 5 ^ 2, 5 << 2, 5 >> 2)
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
